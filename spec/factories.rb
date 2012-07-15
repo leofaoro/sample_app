ce9b920e2ed08f7732863281b2d9514d0c640011
Leonardo Faoro <leo@faoro.org>
2012-07-15 15:00:07 -0700

    after 10.3.1

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,8 +5,13 @@ FactoryGirl.define do
     password "foobar"
     password_confirmation "foobar"
 
-	  factory :admin do
-	    admin true
-	  end
-	end
+    factory :admin do
+      admin true
+    end
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
 end
