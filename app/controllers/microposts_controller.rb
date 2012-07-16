d7291baa8eebb739b1d7fa6ae54d8424a3cab443
Leonardo Faoro <leo@faoro.org>
2012-07-15 20:19:42 -0700

    after 10.3.2

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,8 +2,16 @@ class MicropostsController < ApplicationController
   before_filter :signed_in_user
 
   def create
+      @micropost = current_user.microposts.build(params[:micropost])
+      if @micropost.save
+        flash[:success] = "Micropost created!"
+        redirect_to root_path
+      else
+        @feed_items = []
+        render 'static_pages/home'
+      end
   end
-
+  
   def destroy
   end
 end
